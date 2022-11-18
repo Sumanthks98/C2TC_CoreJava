fbeeab64c7eea550aea06239e821572d159d1952
Sumanthks98 <sumanthks.20.becs@acharya.ac.in>
2022-11-18 19:35:26 +0530

    firstday commit

---
+++ fisrtday/src/fisrtday/First.java
@@ -0,0 +1,11 @@
+package fisrtday;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ fisrtday/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Divya Jyothi
+ *
+ */
+module fisrtday {
+}
